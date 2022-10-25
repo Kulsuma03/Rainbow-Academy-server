f1575011ad5331d8d01e9352ad2677d969ca9460
Kulsuma Akter <kulsumams72@gmail.com>
2022-10-25 16:09:13 +0600

    change course image

--- index.js
+++ index.js
@@ -5,9 +5,40 @@ const port = process.env.port || 5000
 
 app.use(cors());
 
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json')
+
 app.get('/', (req, res) => {
     res.send('assignment 10 server data running');
 });
+
+// load categories 
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+});
+
+// click for categories Item by id
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category = courses.find(c => c.category_id == id);
+    res.send(category);
+})
+
+// load courses
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+// click for course by id
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(c => c.id == id);
+    res.send(course);
+})
 
 app.listen(port, () => {
     console.log('server is running', port)
